acd528c064c3b9499a37708a876d2c93f53cf59d
Joey Parrish <joey.parrish@gmail.com>
2024-12-26 16:43:39 -0800

    Add donation dialog
    Closes #4

--- README.md
+++ README.md
@@ -8,3 +8,25 @@ Hosted at https://ferengi.bible/
 ## ... What?
 
 [It's a Star Trek thing.](https://memory-alpha.fandom.com/wiki/Rules_of_Acquisition)
+
+
+## The Divine Treasury
+
+I always remember the 57th rule: "Good customers are as rare as latinum; treasure them."
+
+If you have enjoyed yourself here, and would like to help pay the bills, you can buy your way into The Divine Treasury!  This website costs $50/year for the domain registration, but if you donate even one slip of latinum, you may send a pull request to add your name and user icon below.
+
+To donate, visit any of the following:
+ - https://liberapay.com/RulesOfAcquisition
+ - https://ko-fi.com/rulesofacquisition
+ - https://buymeacoffee.com/rulesofacquisition
+ - https://github.com/sponsors/joeyparrish
+
+| Supported By |
+| ---- |
+| ![](https://avatars.githubusercontent.com/joeyparrish?size=96) <br> Joey Parrish |
+
+
+![The Divine Treasury](divine-treasury.jpeg)
+
+_Image credit: Meta AI_
--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-(() => {
-  function onhashchange() {
-    if (location.hash) {
-      const pageNumber = parseInt(location.hash.substr(1), 10);
-      const input = document.getElementById('i' + pageNumber);
-      if (input) {
-        input.checked = true;
-      }
+function onhashchange() {
+  if (location.hash) {
+    const pageNumber = parseInt(location.hash.substr(1), 10);
+    const input = document.getElementById('i' + pageNumber);
+    if (input) {
+      input.checked = true;
     }
   }
+}
 
-  document.addEventListener('DOMContentLoaded', onhashchange);
-  window.addEventListener('hashchange', onhashchange);
-})();
+document.addEventListener('DOMContentLoaded', onhashchange);
+window.addEventListener('hashchange', onhashchange);
+
+function sponsor(event) {
+  event.preventDefault();
+  sponsorDialog.classList.add('shown');
+  return false;
+}
+
+function unsponsor(event) {
+  event.preventDefault();
+  sponsorDialog.classList.remove('shown');
+  return false;
+}
