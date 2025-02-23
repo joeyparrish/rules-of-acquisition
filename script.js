51c89e01bc846aaf61a852d1e195505bd8675412
Joey Parrish <joey.parrish@gmail.com>
2025-02-22 19:03:35 -0800

    Stop affecting history
    Closes #7

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+// Change the page and the URL, without affecting browser history.
+function go(pageNumber) {
+  history.replaceState(null, '', '#' + (pageNumber || ''));
+
+  const input = document.getElementById('i' + pageNumber);
+  if (input) {
+    input.checked = true;
+  }
+}
+
+// Only called on explicit URL bar changes and on page load.
+// Loads the page called for by the URL change.
 function onhashchange() {
   if (location.hash) {
     const pageNumber = parseInt(location.hash.substr(1), 10);
-    const input = document.getElementById('i' + pageNumber);
-    if (input) {
-      input.checked = true;
-    }
+    go(pageNumber);
   }
 }
 
